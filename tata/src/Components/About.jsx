e52571a6ecb99b91f1116704ae9a448dbb48b023
Utkarsh Shukla <utkarshshukla.cse@gmail.com>
2025-04-21 16:31:51 +0530

    Update About.jsx

--- tata/src/Components/About.jsx
+++ tata/src/Components/About.jsx
@@ -146,12 +146,6 @@ const About = () => {
 
         </p> 
 
-        <p className="text-gray-300 leading-relaxed mt-4"> 
-
-          My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. 
-
-        </p> 
-
       </div> 
 
       <h2 className="text-3xl font-bold text-white mb-6">What I'm Doing</h2> 
